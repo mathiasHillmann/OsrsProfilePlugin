b90cc43ae0b2ce19177c7dc4378bcd57482e3b9e
Mathias Hillmann <mathias.hillmann@gmail.com>
2023-04-29 19:06:09 -0300

    init plugin

--- src/main/java/com/osrsprofile/OsrsProfilePlugin.java
+++ src/main/java/com/osrsprofile/OsrsProfilePlugin.java
@@ -1,4 +1,4 @@
-package com.example;
+package com.osrsprofile;
 
 import com.google.inject.Provides;
 import javax.inject.Inject;
@@ -14,15 +14,15 @@ import net.runelite.client.plugins.PluginDescriptor;
 
 @Slf4j
 @PluginDescriptor(
-	name = "Example"
+	name = "OsrsProfilePlugin"
 )
-public class ExamplePlugin extends Plugin
+public class OsrsProfilePlugin extends Plugin
 {
 	@Inject
 	private Client client;
 
 	@Inject
-	private ExampleConfig config;
+	private OsrsProfileConfig config;
 
 	@Override
 	protected void startUp() throws Exception
@@ -46,8 +46,8 @@ public class ExamplePlugin extends Plugin
 	}
 
 	@Provides
-	ExampleConfig provideConfig(ConfigManager configManager)
+	OsrsProfileConfig provideConfig(ConfigManager configManager)
 	{
-		return configManager.getConfig(ExampleConfig.class);
+		return configManager.getConfig(OsrsProfileConfig.class);
 	}
 }
